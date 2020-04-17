5e7a294d2e2bb1a09065c3075053fdcd7b17cf72
Yoko <baghram0001@gmail.com>
2020-04-17 20:23:01 +0700

    deploy

--- config/config.js
+++ config/config.js
@@ -1,6 +1,11 @@
 const env = process.env.NODE_ENV || 'development'
 
 switch (env) {
+    case 'development' :
+        require('dotenv').config({
+            path: process.cwd() + '/.env'
+        })
+        break;
     case 'test':
         require('dotenv').config({
             path: process.cwd() +'/.env.test'
