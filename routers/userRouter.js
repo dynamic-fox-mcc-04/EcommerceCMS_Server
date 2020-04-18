0721cdca8e77e213fab21a1637c126747e05d39f
AndreRGunawan <andre.graphica@gmail.com>
2020-04-18 17:42:31 +0700

    deploy trial

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,12 +2,6 @@ var express = require('express')
 var router = express.Router()
 const UserController = require("../controllers/userController.js")
 
-router.get('/', function(req,res){ 
-    res.status(200).json({
-        message:'home-domain-connected'
-    })
-})
-
 router.post('/register', UserController.register) // admin sudah seeded, jadinya endpoint ini tidak diperlukan
 router.post('/login', UserController.login)
 
