1619ec48553821c4a34ddc9d4d147b5b9b645666
archiafinno <archiafinno@gmail.com>
2020-04-18 17:48:52 +0700

    archiafinno

--- models/index.js
+++ models/index.js
@@ -15,8 +15,8 @@ if (process.env.DATABASE_URL) {
         dialect: 'postgres', 
         protocol: 'postgres' 
     });
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// if (config.use_env_variable) {
+//     sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
